f2e39c0599c79c35d3e9711b8733a7c084072ab6
Logan Wang <loganxwang@hotmail.com>
2018-10-23 14:07:47 -0700

    added get on notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -1,48 +1,63 @@
 'use strict';
 
 const express = require('express');
+const mongoose = require('mongoose');
+const { MONGODB_URI } = require('../config');
 
 const router = express.Router();
 
 /* ========== GET/READ ALL ITEMS ========== */
 router.get('/', (req, res, next) => {
+  const { searchTerm } = req.query;
 
-  console.log('Get All Notes');
-  res.json([
-    { id: 1, title: 'Temp 1' },
-    { id: 2, title: 'Temp 2' },
-    { id: 3, title: 'Temp 3' }
-  ]);
-
+  const searchTermReg = new RegExp(searchTerm, 'i');
+  let filter = {};
+  if (searchTerm) {
+    filter = { $or: [{ tilte: searchTermReg }, { content: searchTermReg }] };
+  }
+  return Note.find(filter)
+    .sort({ updatedAt: 'desc' })
+    .then(results => {
+      if (results) {
+        res.json(results);
+      } else {
+        next();
+      }
+    })
+    .catch(err => {
+      next(err);
+    });
+  // console.log('Get All Notes');
+  // res.json([
+  //   { id: 1, title: 'Temp 1' },
+  //   { id: 2, title: 'Temp 2' },
+  //   { id: 3, title: 'Temp 3' }
+  // ]);
 });
 
 /* ========== GET/READ A SINGLE ITEM ========== */
 router.get('/:id', (req, res, next) => {
-
   console.log('Get a Note');
   res.json({ id: 1, title: 'Temp 1' });
-
 });
 
 /* ========== POST/CREATE AN ITEM ========== */
 router.post('/', (req, res, next) => {
-
   console.log('Create a Note');
-  res.location('path/to/new/document').status(201).json({ id: 2, title: 'Temp 2' });
-
+  res
+    .location('path/to/new/document')
+    .status(201)
+    .json({ id: 2, title: 'Temp 2' });
 });
 
 /* ========== PUT/UPDATE A SINGLE ITEM ========== */
 router.put('/:id', (req, res, next) => {
-
   console.log('Update a Note');
   res.json({ id: 1, title: 'Updated Temp 1' });
-
 });
 
 /* ========== DELETE/REMOVE A SINGLE ITEM ========== */
 router.delete('/:id', (req, res, next) => {
-
   console.log('Delete a Note');
   res.status(204).end();
 });
