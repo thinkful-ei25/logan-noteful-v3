d01dea6fd6d8283c08b48427927b9a18a1b19d79
Logan Wang <loganxwang@hotmail.com>
2018-10-25 11:35:36 -0700

    seeded folders database

--- utils/seed-database.js
+++ utils/seed-database.js
@@ -3,8 +3,10 @@ const mongoose = require('mongoose');
 
 const { MONGODB_URI } = require('../config');
 const Note = require('../models/note');
+const Folder = require('../models/folder');
 
-const { notes } = require('../db/seed/notes');
+const { folders, notes } = require('../db/seed/data');
+
 
 mongoose
   .connect(
@@ -12,7 +14,13 @@ mongoose
     { useNewUrlParser: true }
   )
   .then(() => mongoose.connection.db.dropDatabase())
-  .then(() => Note.insertMany(notes))
+  .then(() => {
+    return Promise.all([
+      Note.insertMany(notes),
+      Folder.insertMany(folders),
+      Folder.createIndexes(),
+    ]);
+  })
   .then(results => {
     console.info(`Inserted ${results.length} Notes`);
   })
